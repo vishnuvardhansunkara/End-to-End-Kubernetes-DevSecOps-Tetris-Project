6a96b74a01f1189a5cf40e93031d917b42a9e50d
Developers <105216686+vishnuvardhansunkara@users.noreply.github.com>
2024-02-15 18:31:57 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "gitops-terraform-project"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+   encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
